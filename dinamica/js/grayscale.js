06cbd72666f964cf6c10097c98ea9dd4f6858d8a
Lucas Matos de Medeiros <lucas.medeiros@ccc.ufcg.edu.br>
2015-04-24 15:10:53 -0300

    Update grayscale.js

--- dinamica/js/grayscale.js
+++ dinamica/js/grayscale.js
@@ -41,7 +41,7 @@ function init() {
         zoom: 15,
 
         // The latitude and longitude to center the map (always required)
-        center: new google.maps.LatLng(40.6700, -73.9400), // New York
+      // New York
 
         // Disables the default Google Maps UI components
         disableDefaultUI: true,
@@ -160,19 +160,6 @@ function init() {
         }]
     };
 
-    // Get the HTML DOM element that will contain your map 
-    // We are using a div with id="map" seen below in the <body>
-    var mapElement = document.getElementById('map');
-
-    // Create the Google Map using out element and options defined above
-    var map = new google.maps.Map(mapElement, mapOptions);
-
-    // Custom Map Marker Icon - Customize the map-marker.png file to customize your icon
-    var image = 'img/map-marker.png';
-    var myLatLng = new google.maps.LatLng(40.6700, -73.9400);
-    var beachMarker = new google.maps.Marker({
-        position: myLatLng,
-        map: map,
-        icon: image
+    
     });
 }
